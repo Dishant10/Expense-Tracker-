54e776cd95f96aaa49e6b72a839b2e2e3d57d8c1
Dishant Nagpal <dishantnagpal10@gmail.com>
2022-04-29 16:22:21 +0530

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Dishant Nagpal on 29/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
